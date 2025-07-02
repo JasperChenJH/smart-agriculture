8019dc33abebeb984709b994a326f80ded496e55
ChenJH <3272689875@qq.com>
2025-07-02 18:32:16 +0800

    修改info方法直接用保存在线程的id查询用户信息,前端不需要再传参数

--- src/main/java/com/soultalk/service/UserService.java
+++ src/main/java/com/soultalk/service/UserService.java
@@ -6,7 +6,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 public interface UserService {
     //获取该name的用户信息
-    R info(String name);
+    R info();
 
     //更新个人主页信息
     R update(UserPO user, String introduce, MultipartFile photo);
